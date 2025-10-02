d6abc7289c5fe1ddc4f8f0343e0c6f440cae70eb
Kiyumino <pejlmeiil.ns@gmail.com>
2025-10-02 23:39:38 +0300

    fixed

--- NODE.py
+++ NODE.py
@@ -1,6 +1,7 @@
 import math
 
-a = int(input("Введите первое число: "))
-b = int(input("Введите второе число: "))
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
 
 print(math.gcd(a, b))
